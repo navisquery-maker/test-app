4606071caf980194027537d721c618923b332960
navisquery-maker <navisquery@gmail.com>
2026-05-09 08:18:24 +0800

    test push

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')->required(),
+                TextInput::make('email')->email()->required(),  
+            ]);
+    }
+}
